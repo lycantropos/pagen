7e5ccc33177ebd5a062e09ba15c376f358847540
Azat Ibrakov <azatibrakov@gmail.com>
2025-11-20 03:37:13 +0100

    Fix formatting

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 import sys
+from typing import Any
 
-from setuptools import (find_packages,
-                        setup)
+from setuptools import find_packages, setup
 
 project_base_url = 'https://github.com/lycantropos/pagen/'
-parameters = dict(packages=find_packages(exclude=('tests', 'tests.*')),
-                  url=project_base_url,
-                  download_url=project_base_url + 'archive/master.zip')
+parameters: dict[str, Any] = dict(
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    url=project_base_url,
+    download_url=project_base_url + 'archive/master.zip',
+)
 if sys.implementation.name == 'cpython':
     from setuptools_rust import RustExtension
 
     parameters.update(
-            rust_extensions=[RustExtension('pagen._pagen')],
-            zip_safe=False
+        rust_extensions=[RustExtension('pagen._pagen')], zip_safe=False
     )
 setup(**parameters)
