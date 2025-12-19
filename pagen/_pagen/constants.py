af734fb93f94765649f26b1b2d7599b6c71c86e1
Azat Ibrakov <azatibrakov@gmail.com>
2025-12-19 16:42:11 +0100

    Complete members order

--- pagen/_pagen/constants.py
+++ pagen/_pagen/constants.py
@@ -5,13 +5,13 @@ from typing import Final
 
 CHARACTER_CLASS_SPECIAL_CHARACTERS: Final[str] = '-[\\]^'
 COMMON_SPECIAL_CHARACTERS: Final[str] = 'fnrtv'
-DOUBLE_QUOTED_LITERAL_SPECIAL_CHARACTERS: Final[str] = '"\\'
-SINGLE_QUOTED_LITERAL_SPECIAL_CHARACTERS: Final[str] = "'\\"
 COMMON_SPECIAL_CHARACTERS_TRANSLATION_TABLE: Final[Mapping[int, str]] = {
     ord(('\\' + character).encode('utf-8').decode('unicode-escape')): '\\'
     + character
     for character in COMMON_SPECIAL_CHARACTERS
 }
+DOUBLE_QUOTED_LITERAL_SPECIAL_CHARACTERS: Final[str] = '"\\'
+SINGLE_QUOTED_LITERAL_SPECIAL_CHARACTERS: Final[str] = "'\\"
 
 assert len(
     non_special_characters := [
