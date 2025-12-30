e0c58d597794a731f9af3ae0933816b6ab26d28c
Azat Ibrakov <azatibrakov@gmail.com>
2025-12-30 18:17:07 +0100

    Fix formatting

--- pagen/_pagen/constants.py
+++ pagen/_pagen/constants.py
@@ -6,8 +6,9 @@ from typing import Final
 CHARACTER_CLASS_SPECIAL_CHARACTERS: Final[str] = '-[\\]^'
 COMMON_SPECIAL_CHARACTERS: Final[str] = 'fnrtv'
 COMMON_SPECIAL_CHARACTERS_TRANSLATION_TABLE: Final[Mapping[int, str]] = {
-    ord(('\\' + character).encode('utf-8').decode('unicode-escape')): '\\'
-    + character
+    ord(('\\' + character).encode('utf-8').decode('unicode-escape')): (
+        '\\' + character
+    )
     for character in COMMON_SPECIAL_CHARACTERS
 }
 DOUBLE_QUOTED_LITERAL_SPECIAL_CHARACTERS: Final[str] = '"\\'
@@ -17,7 +18,9 @@ assert len(
     non_special_characters := [
         character
         for character in COMMON_SPECIAL_CHARACTERS
-        if len(('\\' + character).encode('unicode-escape').decode('utf-8'))
-        != 1
+        if (
+            len(('\\' + character).encode('unicode-escape').decode('utf-8'))
+            != 1
+        )
     ]
 ), non_special_characters
